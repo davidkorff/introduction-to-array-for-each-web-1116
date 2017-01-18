3ac393b0f0642bddd8292d414a75de6124f1ecf8
davidkorff <davidkorff@gmail.com>
2017-01-18 09:29:33 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function changeCompletely(element, index, array){
+
+}
+
+function doToElementsInArray(array, callback){
+
+}
+
+
+var evens = [0, 2, 4, 6, 8, 10]
+
+evens.forEach(even => {
+  debugger
+  console.log(`${even} is not odd!`)
+})
